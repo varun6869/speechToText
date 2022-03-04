3828710ae4cdfbdb9d1aa033d610324c936ca122
varun6869 <varunkudi@gmail.com>
2022-03-04 15:34:42 +0530

    initial commit

---
+++ test.js
@@ -0,0 +1,200 @@
+// Speech to Text Conversion in React Native – Voice Recognition
+// https://aboutreact.com/speech-to-text-conversion-in-react-native-voice-recognition/
+
+// import React in our code
+import React, {useState, useEffect} from 'react';
+
+// import all the components we are going to use
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  ScrollView,
+} from 'react-native';
+
+// import Voice
+import Voice from 'react-native-voice';
+
+const App = () => {
+  const [pitch, setPitch] = useState('');
+  const [error, setError] = useState('');
+  const [end, setEnd] = useState('');
+  const [started, setStarted] = useState('');
+  const [results, setResults] = useState([]);
+  const [partialResults, setPartialResults] = useState([]);
+
+  useEffect(() => {
+    //Setting callbacks for the process status
+    Voice.onSpeechStart = onSpeechStart;
+    Voice.onSpeechEnd = onSpeechEnd;
+    Voice.onSpeechError = onSpeechError;
+    Voice.onSpeechResults = onSpeechResults;
+    Voice.onSpeechPartialResults = onSpeechPartialResults;
+    Voice.onSpeechVolumeChanged = onSpeechVolumeChanged;
+
+    return () => {
+      //destroy the process after switching the screen
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const onSpeechStart = (e) => {
+    //Invoked when .start() is called without error
+    console.log('onSpeechStart: ', e);
+    setStarted('√');
+  };
+
+  const onSpeechEnd = (e) => {
+    //Invoked when SpeechRecognizer stops recognition
+    console.log('onSpeechEnd: ', e);
+    setEnd('√');
+  };
+
+  const onSpeechError = (e) => {
+    //Invoked when an error occurs.
+    console.log('onSpeechError: ', e);
+    setError(JSON.stringify(e.error));
+  };
+
+  const onSpeechResults = (e) => {
+    //Invoked when SpeechRecognizer is finished recognizing
+    console.log('onSpeechResults: ', e);
+    setResults(e.value);
+  };
+
+  const onSpeechPartialResults = (e) => {
+    //Invoked when any results are computed
+    console.log('onSpeechPartialResults: ', e);
+    setPartialResults(e.value);
+  };
+
+  const onSpeechVolumeChanged = (e) => {
+    //Invoked when pitch that is recognized changed
+    console.log('onSpeechVolumeChanged: ', e);
+    setPitch(e.value);
+  };
+
+  const startRecognizing = async () => {
+    //Starts listening for speech for a specific locale
+    try {
+      await Voice.start('en-US');
+      setPitch('');
+      setError('');
+      setStarted('');
+      setResults([]);
+      setPartialResults([]);
+      setEnd('');
+    } catch (e) {
+      //eslint-disable-next-line
+      console.error(e);
+    }
+  };
+
+  const stopRecognizing = async () => {
+    //Stops listening for speech
+    try {
+      await Voice.stop();
+    } catch (e) {
+      //eslint-disable-next-line
+      console.error(e);
+    }
+  };
+
+  const cancelRecognizing = async () => {
+    //Cancels the speech recognition
+    try {
+      await Voice.cancel();
+    } catch (e) {
+      //eslint-disable-next-line
+      console.error(e);
+    }
+  };
+
+  const destroyRecognizer = async () => {
+    //Destroys the current SpeechRecognizer instance
+    try {
+      await Voice.destroy();
+      setPitch('');
+      setError('');
+      setStarted('');
+      setResults([]);
+      setPartialResults([]);
+      setEnd('');
+    } catch (e) {
+      //eslint-disable-next-line
+      console.error(e);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+        <View style={styles.firstrow}>
+            <Text style={styles.titleText}>
+                 SPEECH TO TEXT 
+           </Text> 
+        </View>
+
+        <View style={styles.thirdrow}>
+        <ScrollView style={{marginBottom: 42}}>
+          {results.map((result, index) => {
+            return (
+              <Text
+                key={`result-${index}`}
+                style={styles.textStyle}>
+                {result}
+              </Text>
+            );
+          })}
+        </ScrollView>
+        </View>
+        <View style={styles.fourthrow}></View>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1f2041',
+  },
+  firstrow: {
+    flex: 0.2,
+    backgroundColor: "#b5179e",
+    
+  },
+  titleText: {
+    fontSize: 22,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color:"#ffffff",
+    margin:"1%",
+    padding:10,
+  },
+  secondrow: {
+    flex: 0.5,
+    backgroundColor: "#7209b7"
+  },
+  imageButton: {
+    width: 50,
+    height: 50,
+  },
+  textStyle: {
+    textAlign: 'center',
+    padding: 12,
+    color:"#ffffff"
+  },
+  thirdrow: {
+    flex: 1,
+    backgroundColor: "#560bad"
+  },
+  fourthrow: {
+    flex: 1,
+    backgroundColor: "#480ca8"
+  }
+
+});
